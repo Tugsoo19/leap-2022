fe4217687053c0a19e5d3d055425abd7d30d84dc
Morning <morning@Mstarss-iMac-2.local>
2022-12-01 09:24:03 +0800

    day12 ex last

--- Day-12/js/exercise-tip.js
+++ Day-12/js/exercise-tip.js
@@ -1,49 +1,49 @@
-// //------------------exercise TIP -----------------------//
+//------------------exercise TIP -----------------------//
 
 
-// const tip1 = 3000;
-// const tip2 = 27500;
-// const tip3 = 100000;
+const tip1 = 3000;
+const tip2 = 27500;
+const tip3 = 100000;
 
-// let tipA = (5000 <= tip1 && tip1 <= 30000)? (tip1 + (tip1 * 0.15)) : (tip1 + (tip1 * 0.20));
-// let tipB = (5000 <= tip2 && tip2 <= 30000)? (tip2 + (tip2 * 0.15)) : (tip2 + (tip2 * 0.20));
-// let tipC = (5000 <= tip3 && tip3 <= 30000)? (tip3 + (tip3 * 0.15)) : (tip3 + (tip3 * 0.20));
+let tipA = (5000 <= tip1 && tip1 <= 30000)? (tip1 + (tip1 * 0.15)) : (tip1 + (tip1 * 0.20));
+let tipB = (5000 <= tip2 && tip2 <= 30000)? (tip2 + (tip2 * 0.15)) : (tip2 + (tip2 * 0.20));
+let tipC = (5000 <= tip3 && tip3 <= 30000)? (tip3 + (tip3 * 0.15)) : (tip3 + (tip3 * 0.20));
 
-// console.log(tipA);
-// console.log(tipB);
-// console.log(tipC);
+console.log(tipA);
+console.log(tipB);
+console.log(tipC);
 
-// let sum = tipA + tipB + tipC;
-// console.log(sum);
+let sum = tipA + tipB + tipC;
+console.log(sum);
 
-// //----------------- Students exam result exercise ----------------------//
+//----------------- Students exam result exercise ----------------------//
 
-// const studentName = prompt('Give me your name');
-// const studentPoint = Number(prompt('Give me your score'));
+const studentName = prompt('Give me your name');
+const studentPoint = Number(prompt('Give me your score'));
 
 
-// let studentResult = '0 - 100ийн хооронд оноо байх ёстой';
+let studentResult = '0 - 100ийн хооронд оноо байх ёстой';
 
     
-// if (90 <= studentPoint && studentPoint <= 100) {
-//     studentResult = ' маш сайн';
-// } else if (80 <= studentPoint && studentPoint < 90){
-//     studentResult = ' сайн';
-// } else if (70 <= studentPoint && studentPoint < 80){
-//     studentResult = ' дунд';
-// } else if (60 <= studentPoint && studentPoint < 70){
-//     studentResult = ' хангалттай';
-// } else if (60 > studentPoint){
-//     studentResult = ' маш муу';
-// }
-// else {
-//     console.log('buruu');
-// }
+if (90 <= studentPoint && studentPoint <= 100) {
+    studentResult = ' маш сайн';
+} else if (80 <= studentPoint && studentPoint < 90){
+    studentResult = ' сайн';
+} else if (70 <= studentPoint && studentPoint < 80){
+    studentResult = ' дунд';
+} else if (60 <= studentPoint && studentPoint < 70){
+    studentResult = ' хангалттай';
+} else if (60 > studentPoint){
+    studentResult = ' маш муу';
+}
+else {
+    console.log('buruu');
+}
 
-// alert (studentName + '-' + studentPoint + '-' + studentResult);
+alert (studentName + '-' + studentPoint + '-' + studentResult);
 
-// console.log(`${studentName} - `);
-// console.log(`${studentName} - ${studentPoint} - ${studentResult}`);
+console.log(`${studentName} - `);
+console.log(`${studentName} - ${studentPoint} - ${studentResult}`);
 
 
 
@@ -75,3 +75,29 @@ if (!Number.isInteger(Number(regionCode)) && regionNumber == 976 && Number.isInt
     console.log("та монгол улсын утасны дугаарыг буруу орууллаа");
 }
 
+
+//-------- Validate Registration number ------------//
+
+const registerNumber = prompt("Register Number"); 
+// console.log(registerNumber.length);
+// if (registerNumber.length != 10) {
+//     alert ("The Register number length is not correct.")
+// } else {
+//     alert ("the Register number length is correct.")
+// }
+
+
+console.log(registerNumber.length);
+console.log(registerNumber.substr(0,2));
+console.log(registerNumber.substr(2,9));
+
+const registerChar = registerNumber.substr(0,2);
+const registerNum = registerNumber.substr(2,9);
+
+
+if (!Number.isInteger(Number(registerChar)) && Number.isInteger(Number(registerNum)) && registerNumber.length == 10) {
+    console.log('Your register characters are correct');
+    
+} else {
+    console.log('Your register characters are wrong');
+}
