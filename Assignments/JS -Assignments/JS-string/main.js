31095574386a2909b3f93af85c5a6e9c72c22065
Tugsoo <tugsoo@Tugsoo-MacBook.local>
2022-12-02 08:54:15 +0800

    update

---
+++ Assignments/JS -Assignments/JS-string/main.js
@@ -0,0 +1,18 @@
+//------------ escape sequences -------------//
+
+let name = prompt("Neree oruulna uu");
+let age = prompt("Nasaa oruulna uu");
+const a = ("Hello ")
+const b = ("I am ")
+const c = (" years old. This is the new line with tab and some \"backslashes\".");
+
+console.log(a+b+ name + "." + b + age + c);
+
+
+//------------ simple assignment ------------//
+//----No.1-----//
+let firstName = prompt ("Insert First name");
+let lastName = prompt ("Insert Last name");
+
+console.log(firstName.length);
+console.log(lastName.length);
---
+++ Day-13/js/assign.js
@@ -0,0 +1,31 @@
+// -------------- tegsh toog oloh --------------//
+
+// let count = 0; 
+
+// console.log("loop start");
+// while (count<= 100 ) {
+//     console.log(count);
+//     count = count + 2;  
+// }
+// console.log("loop ended")
+
+// --------------- Palindromic-number --------------//
+
+let n = prompt("palendrom esehiig shalgah too oruulna uu");
+console.log(n);
+let urt = Number(n.length);
+console.log(n.length)
+console.log(typeof(n.length));
+
+ if (!(urt % 2) == 0) {
+    console.log("sondgoi orontoi too");
+    
+
+ }
+ else {console.log("tegsh orontoi too");
+}
+
+
+
+
+
