af30af6cfe2ae751b799e1f5f9a7962ef1f615d3
Tugsoo <tugsoo@Tugsoo-MacBook.local>
2022-12-08 14:09:53 +0800

    array method ex3

---
+++ Assignments/JS - methods/ex03/ex03.js
@@ -0,0 +1,11 @@
+// ex-03 Reverse a number 
+
+console.log("reverse a number");
+
+let input = prompt("Toogoo oruulna uu")
+
+const split = input.split('');
+
+const reverse= split.reverse();
+console.log(split.join(''));
+
